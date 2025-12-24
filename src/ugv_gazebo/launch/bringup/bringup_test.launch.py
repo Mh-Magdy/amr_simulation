804eb596a50dd7c00c8b5cc93e5e0de37ee47249
Goldapple0905 <danielpcollins02@gmail.com>
2025-12-24 08:48:57 +0200

    Add navigation edits

--- src/ugv_gazebo/launch/bringup/bringup_test.launch.py
+++ src/ugv_gazebo/launch/bringup/bringup_test.launch.py
@@ -1,61 +1,54 @@
 #!/usr/bin/env python3
 #
-# Copyright 2019 ROBOTIS CO., LTD.
+# bringup_test.launch.py
+# Fully working ROS 2 launch for UGV in Gazebo (Humle / ROS 2)
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Authors: Joep Tool
 
 import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
-    # Get the directory of the launch file
-    launch_file_dir = os.path.join(get_package_share_directory('ugv_gazebo'), 'launch/bringup')
-    # Get the directory of the gazebo_ros package
+    # -----------------------------
+    # Package directories
+    # -----------------------------
+    pkg_ugv = get_package_share_directory('ugv_gazebo')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
-    # Get the use_sim_time parameter from the launch file
+    launch_file_dir = os.path.join(pkg_ugv, 'launch', 'bringup')
+    world_file = os.path.join(pkg_ugv, 'worlds', 'myworld3.world')
+    robot_model_file = os.path.join(pkg_ugv, 'models', 'ugv_rover', 'model.sdf')
+
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
 
-    # Get the world file
-    world = os.path.join(
-        get_package_share_directory('ugv_gazebo'),
-        'worlds',
-        'ugv_world.world'
-    )
-
-    # Include the gzserver launch file
+    # -----------------------------
+    # Gazebo server
+    # -----------------------------
     gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
-        )
+        ),
+        launch_arguments={'world': world_file}.items()
     )
 
-    # Include the gzclient launch file
+    # -----------------------------
+    # Gazebo client
+    # -----------------------------
     gzclient_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
         )
     )
-            
-    # Include the robot_state_publisher launch file
+
+    # -----------------------------
+    # Robot State Publisher
+    # -----------------------------
     robot_state_publisher_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(launch_file_dir, 'robot_state_publisher.launch.py')
@@ -63,20 +56,28 @@ def generate_launch_description():
         launch_arguments={'use_sim_time': use_sim_time}.items()
     )
 
-    # Include the spawn_ugv launch file
-    spawn_ugv_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(launch_file_dir, 'spawn_ugv.launch.py')
-        )
+    # -----------------------------
+    # Spawn UGV after Gazebo starts (TimerAction)
+    # -----------------------------
+    spawn_ugv_cmd = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-entity', 'ugv_rover', '-file', robot_model_file],
+        output='screen'
     )
 
-    # Create a launch description
-    ld = LaunchDescription()
+    spawn_ugv_delayed = TimerAction(
+        period=5.0,  # delay in seconds to allow Gazebo to start
+        actions=[spawn_ugv_cmd]
+    )
 
-    # Add the commands to the launch description
+    # -----------------------------
+    # Build launch description
+    # -----------------------------
+    ld = LaunchDescription()
     ld.add_action(gzserver_cmd)
     ld.add_action(gzclient_cmd)
     ld.add_action(robot_state_publisher_cmd)
-    ld.add_action(spawn_ugv_cmd)
+    ld.add_action(spawn_ugv_delayed)
 
     return ld
